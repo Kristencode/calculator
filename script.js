5714f90785009e321bcd2fd36e98bfdb908e8e2d
Kristen <harunakristen@gmail.com>
2025-03-17 13:22:32 +0000

    final commit

--- script.js
+++ script.js
@@ -22,8 +22,28 @@ document.getElementById("del").addEventListener("click", function () {
 document.getElementById("cal").addEventListener("click", function () {
   try {
     let input = document.getElementById("input");
-    input.value = eval(input.value);
+    input.value = math.evaluate(input.value);
   } catch (e) {
     input.value = "Error";
   }
+});
+
+// whats the difference between eval() & math.eval
+
+// square root
+
+document.getElementById("root").addEventListener("click", function () {
+  let input = document.getElementById("input");
+  input.value = math.sqrt(input.value);
+
+  if (!isNaN(input)) {
+    input.value = math.sqrt(input.value);
+  }
+});
+
+// raise to the power of 2
+
+document.getElementById("square").addEventListener("click", function () {
+  let input = document.getElementById("input");
+  input.value = input.value * input.value;
 });
